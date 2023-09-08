762b33a994f6f7ed4d5b0e21cb6c3153fa8e9158
root <root@localhost>
2023-09-08 15:03:35 +0000

    Add seed file with books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+books = Book.create([{ name: "LOTR" }, { name: "Fangorn Fanatics" }, { name: "Mirkwood Murders" }, { name: "Shire Shenanigans" }, { name: "Gollums Greats" }])
+
