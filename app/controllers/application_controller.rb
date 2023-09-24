28b84f465468f45523aedbd863bc88a1b70c957e
jonas <jonasaustinland@tamu.edu>
2023-09-24 18:20:23 -0500

    Non members can now create accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_admin!
-end
+    before_action :authenticate_admin!, unless: :skip_authentication?
+  
+    private
+  
+    def skip_authentication?
+      # return true
+      if (request.path == new_account_members_path) || (request.path == create_account_members_path)
+        return true  
+      else
+        return false 
+      end
+    end
+  end
+  
