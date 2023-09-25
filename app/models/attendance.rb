682d81a8ec8b5c70c23773d5d27b28b10ef640a2
Daniel14676 <112496278+Daniel14676@users.noreply.github.com>
2023-09-24 19:30:20 -0500

    attendance form

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,4 @@
 class Attendance < ApplicationRecord
   belongs_to :member
   belongs_to :event
-  validates :attended, inclusion: { in: [true, false] }
 end
--- db/migrate/20230923223043_create_attendances.rb
+++ db/migrate/20230923223043_create_attendances.rb
@@ -3,7 +3,6 @@ class CreateAttendances < ActiveRecord::Migration[7.0]
     create_table :attendances do |t|
       t.references :member, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
-      t.boolean :attended
 
       t.timestamps
     end
