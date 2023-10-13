a412fbca952280efc1bcb73b137de9080a2b0924
jonas <jonasaustinland@tamu.edu>
2023-10-13 09:12:35 -0500

    Updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-books = Book.create([{ name: "LOTR" }, { name: "Fangorn Fanatics" }, { name: "Mirkwood Murders" }, { name: "Shire Shenanigans" }, { name: "Gollums Greats" }])
+books = Book.create([{ name: "LOTR" }, { name: "Fangorn Fanatics" }, { name: "Mirkwood Murders" }, 
+{ name: "Shire Shenanigans" }, { name: "Gollums Greats" }])
 
 members_data = [
-  { first_name: 'Jonas', preferred_name: 'Jonas', last_name: 'Land', email: "jonasaustinland@tamu.edu", is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Adam', preferred_name: 'Adam', last_name: 'Pinto', email: "adampinto@tamu.edu", is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Daniel', preferred_name: 'Daniel', last_name: 'McSween', email: "daniel14676@tamu.edu", is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Isabelle', preferred_name: 'Isabelle', last_name: 'Grimesay', email: "isabelle.grimesey@tamu.edu", is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Mark', preferred_name: 'Mark', last_name: 'Baca', email: "markbaca42@tamu.edu", is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Jonas', preferred_name: 'Jonas', last_name: 'Land', email: "jonasaustinland@gmail.com", is_member: true, is_admin: false, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Olukayode', preferred_name: 'Olukayode', last_name: 'Apata', email: "apata.olukayode@tamu.edu", is_member: true, is_admin: true, bio: 'Some bio text', contact: 'apata.olukayode@tamu.edu', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Test', preferred_name: 'Test', last_name: 'Officer', email: "dclftest@gmail.com", is_member: true, is_admin: true, bio: 'Some bio text', contact: '', photo_file_name: 'john.jpg', role: 'Admin' },
-  { first_name: 'Test', preferred_name: 'Test', last_name: 'Member', email: "dlcftest@gmail.com", is_member: true, is_admin: false, bio: 'Some bio text', contact: '', photo_file_name: 'john.jpg', role: 'Admin' },
+  { first_name: 'Jonas', preferred_name: 'Jonas', last_name: 'Land', 
+  email: "jonasaustinland@tamu.edu", is_member: true, is_admin: true,
+   bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', role: 'Admin' },
+  { first_name: 'Adam', preferred_name: 'Adam', last_name: 'Pinto', email: "adampinto@tamu.edu", 
+  is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', 
+  role: 'Admin' },
+  { first_name: 'Daniel', preferred_name: 'Daniel', last_name: 'McSween', email: "daniel14676@tamu.edu", 
+  is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', 
+  role: 'Admin' },
+  { first_name: 'Isabelle', preferred_name: 'Isabelle', last_name: 'Grimesay', email: "isabelle.grimesey@tamu.edu", 
+  is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg', 
+  role: 'Admin' },
+  { first_name: 'Mark', preferred_name: 'Mark', last_name: 'Baca', email: "markbaca42@tamu.edu",
+   is_member: true, is_admin: true, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg',
+    role: 'Admin' },
+  { first_name: 'Jonas', preferred_name: 'Jonas', last_name: 'Land', email: "jonasaustinland@gmail.com",
+   is_member: true, is_admin: false, bio: 'Some bio text', contact: 'Contact info', photo_file_name: 'john.jpg',
+    role: 'Admin' },
+  { first_name: 'Olukayode', preferred_name: 'Olukayode', last_name: 'Apata', email: "apata.olukayode@tamu.edu",
+   is_member: true, is_admin: true, bio: 'Some bio text', contact: 'apata.olukayode@tamu.edu', 
+   photo_file_name: 'john.jpg', role: 'Admin' },
+  { first_name: 'Test', preferred_name: 'Test', last_name: 'Officer', email: "dclftest@gmail.com", 
+  is_member: true, is_admin: true, bio: 'Some bio text', contact: '', photo_file_name: 'john.jpg',
+   role: 'Admin' },
+  { first_name: 'Test', preferred_name: 'Test', last_name: 'Member', email: "dlcftest@gmail.com", 
+  is_member: true, is_admin: false, bio: 'Some bio text', contact: '', photo_file_name: 'john.jpg', 
+  role: 'Admin' },
   
   
 
