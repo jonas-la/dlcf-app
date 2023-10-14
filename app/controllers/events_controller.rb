e50a07193962a9f043f2d262645519c96bd83387
markbacaTamu <markbaca42@tamu.edu>
2023-10-14 13:32:58 -0500

    added passwords

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -86,6 +86,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:event_name, :location, :start_time, :end_time, :description)
+      params.require(:event).permit(:event_name, :location, :start_time, :end_time, :description, :password)
     end
 end
